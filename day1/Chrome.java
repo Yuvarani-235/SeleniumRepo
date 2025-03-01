a3e77398692502a4bc770609637ef582ba2be5aa
Yuvarani-235 <yuvsan235@gmail.com>
2025-03-01 21:57:05 +0530

    Week1

---
+++ day1/Chrome.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println("Browser Version= "+version+",\n"+"Browser developer= "+developer+",\n"+"Beta Value= "+isBeta+",\n"+"Release year= "+releaseYear+",\n"+"Shortcut key= "+shortcutKey);
+	}
+
+}
---
+++ day1/FireFox.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class FireFox {
+
+	public static void main(String[] args) {
+		
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+
+		System.out.println("Browser Version= "+browserVersion+", Browser Name= "+browserName+", Visibility= "+isVisible+", Release year= "+releaseYear+", Browser Logo= "+browserLogo);
+	}
+
+}
